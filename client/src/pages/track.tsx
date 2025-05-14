5730435b4b77ffbef94df5ea80730bc3d57f121a
Jan Arnel Banaag <jhonarnel476@gmail.com>
2025-05-14 11:29:10 +0800

    added GET_TRACK query

--- client/src/pages/track.tsx
+++ client/src/pages/track.tsx
@@ -4,6 +4,30 @@ import { useQuery } from "@apollo/client";
 import { Layout, QueryResult } from "../components";
 import { useParams } from "react-router-dom";
 
+export const GET_TRACK = gql(`
+  query GetTrack($trackId: ID!) {
+    track(id: $trackId) {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+      numberOfViews
+      modules {
+        id
+        title
+        length
+      }
+      description
+    }
+  }
+`);
+
 const Track = () => {
 	const { trackId = "" } = useParams();
 
